356b4056156634f0809dd12e72cbb538f47da9dc
jeff <jeff@fallever.com>
2014-11-13 15:11:15 +0800

    增加对所有菜单事件的支持

---
+++ wechat/models.py
@@ -0,0 +1,167 @@
+#encoding=utf-8
+
+from xml.dom import minidom
+import collections
+import time
+
+
+def kv2element(key, value, doc):
+    ele = doc.createElement(key)
+    if isinstance(value, str) or isinstance(value, unicode):
+        data = doc.createCDATASection(value)
+        ele.appendChild(data)
+    else:
+        text = doc.createTextNode(str(value))
+        ele.appendChild(text)
+    return ele
+
+
+def fields2elements(tupleObj, enclose_tag=None, doc=None):
+    if enclose_tag:
+        xml = doc.createElement(enclose_tag)
+        for key in tupleObj._fields:
+            ele = kv2element(key, getattr(tupleObj, key), doc)
+            xml.appendChild(ele)
+        return xml
+    else:
+        return [kv2element(key, getattr(tupleObj, key), doc)
+                for key in tupleObj._fields]
+
+
+class WxRequest(object):
+
+    def __init__(self, xml=None):
+        if not xml:
+            return
+        doc = minidom.parseString(xml)
+        params = [ele for ele in doc.childNodes[0].childNodes
+                  if isinstance(ele, minidom.Element)]
+        for param in params:
+            if param.childNodes:
+                text = param.childNodes[0]
+                self.__dict__.update({param.tagName: text.data})
+            else:
+                self.__dict__.update({param.tagName: ''})
+
+
+class WxResponse(object):
+
+    def __init__(self, request):
+        self.CreateTime = long(time.time())
+        self.ToUserName = request.FromUserName
+        self.FromUserName = request.ToUserName
+        self.Extra = {}
+
+    def as_xml(self):
+        doc = minidom.Document()
+        xml = doc.createElement('xml')
+        doc.appendChild(xml)
+        xml.appendChild(kv2element('ToUserName', self.ToUserName, doc))
+        xml.appendChild(kv2element('FromUserName', self.FromUserName, doc))
+        xml.appendChild(kv2element('CreateTime', self.CreateTime, doc))
+        xml.appendChild(kv2element('MsgType', self.MsgType, doc))
+        contents = self.content_nodes(doc)
+        if isinstance(contents, list) or isinstance(contents, tuple):
+            for content in contents:
+                xml.appendChild(content)
+        else:
+            xml.appendChild(contents)
+        if self.Extra:
+            for key, value in self.Extra.iteritems():
+                xml.appendChild(kv2element(key, value, doc))
+        return doc.toxml()
+
+
+WxMusic = collections.namedtuple('WxMusic',
+                                 'Title Description MusicUrl HQMusicUrl')
+WxArticle = collections.namedtuple('WxArticle',
+                                   'Title Description PicUrl Url')
+WxImage = collections.namedtuple('WxImage', 'MediaId')
+WxVoice = collections.namedtuple('WxVoice', 'MediaId')
+WxVideo = collections.namedtuple('WxVideo',
+                                 'MediaId Title Description')
+WxLink = collections.namedtuple('WxLink', 'Title Description Url')
+
+
+class WxEmptyResponse(WxResponse):
+
+    def __init__(self):
+        pass
+
+    def as_xml(self):
+        return ''
+
+
+class WxTextResponse(WxResponse):
+
+    MsgType = 'text'
+
+    def __init__(self, text, request):
+        super(WxTextResponse, self).__init__(request)
+        self.text = text
+
+    def content_nodes(self, doc):
+        return kv2element('Content', self.text, doc)
+
+
+class WxCompoundResponse(WxResponse):
+
+    MsgType = ''
+    Tag = ''
+
+    def __init__(self, data, request):
+        super(WxCompoundResponse, self).__init__(request)
+        self.data
+
+    def content_nodes(self, doc):
+        return self.fields2elements(self.data, self.Tag, doc)
+
+
+class WxImageResponse(WxCompoundResponse):
+
+    MsgType = 'image'
+    Tag = 'Image'
+
+
+class WxVoiceResponse(WxCompoundResponse):
+
+    MsgType = 'voice'
+    Tag = 'Voice'
+
+
+class WxVideoResponse(WxCompoundResponse):
+
+    MsgType = 'video'
+    Tag = 'Video'
+
+
+class WxMusicResponse(WxResponse):
+
+    MsgType = 'music'
+    Tag = 'Music'
+
+
+class WxNewsResponse(WxResponse):
+
+    MsgType = 'news'
+
+    def __init__(self, articles, request):
+        super(WxNewsResponse, self).__init__(request)
+        if isinstance(articles, list) or isinstance(articles, tuple):
+            self.articles = articles
+        else:
+            self.articles = [articles]
+
+    def content_nodes(self, doc):
+        count = kv2element('ArticleCount', len(self.articles), doc)
+        articles = doc.createElement('Articles')
+        for article in self.articles:
+            articles.appendChild(fields2elements(article, 'item', doc))
+        return count, articles
+
+
+class APIError(object):
+
+    def __init__(self, code, msg):
+        self.code = code
+        self.message = msg
--- wechat/official.py
+++ wechat/official.py
@@ -1,7 +1,5 @@
 # encoding=utf-8
-from xml.dom import minidom
-import collections
-import time
+
 from hashlib import sha1
 import requests
 import simplejson as json
@@ -10,124 +8,16 @@ import shutil
 import os
 from .crypt import WXBizMsgCrypt
 
+from .models import WxRequest, WxResponse
+from .models import WxMusic, WxArticle, WxImage, WxVoice, WxVideo, WxLink
+from .models import WxTextResponse, WxImageResponse, WxVoiceResponse,\
+    WxVideoResponse, WxMusicResponse, WxNewsResponse, APIError, WxEmptyResponse
 
-def kv2element(key, value, doc):
-    ele = doc.createElement(key)
-    if isinstance(value, str) or isinstance(value, unicode):
-        data = doc.createCDATASection(value)
-        ele.appendChild(data)
-    else:
-        text = doc.createTextNode(str(value))
-        ele.appendChild(text)
-    return ele
-
-
-def fields2elements(tupleObj, enclose_tag=None, doc=None):
-    if enclose_tag:
-        xml = doc.createElement(enclose_tag)
-        for key in tupleObj._fields:
-            ele = kv2element(key, getattr(tupleObj, key), doc)
-            xml.appendChild(ele)
-        return xml
-    else:
-        return [kv2element(key, getattr(tupleObj, key), doc)
-                for key in tupleObj._fields]
-
-
-class WxRequest(object):
-
-    def __init__(self, xml=None):
-        if not xml:
-            return
-        doc = minidom.parseString(xml)
-        params = [ele for ele in doc.childNodes[0].childNodes
-                  if isinstance(ele, minidom.Element)]
-        for param in params:
-            if param.childNodes:
-                text = param.childNodes[0]
-                self.__dict__.update({param.tagName: text.data})
-            else:
-                self.__dict__.update({param.tagName: ''})
-
-
-class WxResponse(object):
-
-    def __init__(self, request):
-        self.CreateTime = long(time.time())
-        self.FuncFlag = 0
-        self.ToUserName = request.FromUserName
-        self.FromUserName = request.ToUserName
-        self.Extra = {}
-
-    def as_xml(self):
-        doc = minidom.Document()
-        xml = doc.createElement('xml')
-        doc.appendChild(xml)
-        xml.appendChild(kv2element('ToUserName', self.ToUserName, doc))
-        xml.appendChild(kv2element('FromUserName', self.FromUserName, doc))
-        xml.appendChild(kv2element('CreateTime', self.CreateTime, doc))
-        xml.appendChild(kv2element('MsgType', self.MsgType, doc))
-        contents = self.content_nodes(doc)
-        if isinstance(contents, list) or isinstance(contents, tuple):
-            for content in contents:
-                xml.appendChild(content)
-        else:
-            xml.appendChild(contents)
-        if self.Extra:
-            for key, value in self.Extra.iteritems():
-                xml.appendChild(kv2element(key, value, doc))
-        xml.appendChild(kv2element('FuncFlag', self.FuncFlag, doc))
-        return doc.toxml()
-
-
-WxMusic = collections.namedtuple('WxMusic',
-                                 'Title Description MusicUrl HQMusicUrl')
-WxArticle = collections.namedtuple('WxArticle',
-                                   'Title Description PicUrl Url')
-WxLink = collections.namedtuple('WxLink', 'Title Description Url')
-
-
-class WxTextResponse(WxResponse):
-
-    MsgType = 'text'
-
-    def __init__(self, text, request):
-        super(WxTextResponse, self).__init__(request)
-        self.text = text
-
-    def content_nodes(self, doc):
-        return kv2element('Content', self.text, doc)
-
-
-class WxMusicResponse(WxResponse):
-
-    MsgType = 'music'
-
-    def __init__(self, music, request):
-        super(WxMusicResponse, self).__init__(request)
-        self.music = music
-
-    def content_nodes(self, doc):
-        return fields2elements(self.music, 'Music', doc)
-
-
-class WxNewsResponse(WxResponse):
-
-    MsgType = 'news'
-
-    def __init__(self, articles, request):
-        super(WxNewsResponse, self).__init__(request)
-        if isinstance(articles, list) or isinstance(articles, tuple):
-            self.articles = articles
-        else:
-            self.articles = [articles]
-
-    def content_nodes(self, doc):
-        count = kv2element('ArticleCount', len(self.articles), doc)
-        articles = doc.createElement('Articles')
-        for article in self.articles:
-            articles.appendChild(fields2elements(article, 'item', doc))
-        return count, articles
+__all__ = ['WxRequest', 'WxResponse', 'WxMusic', 'WxArticle', 'WxImage',
+           'WxVoice', 'WxVideo', 'WxLink', 'WxTextResponse',
+           'WxImageResponse', 'WxVoiceResponse', 'WxVideoResponse',
+           'WxMusicResponse', 'WxNewsResponse', 'WxApplication',
+           'WxEmptyResponse', 'WxApi', 'APIError']
 
 
 class WxApplication(object):
@@ -225,7 +115,13 @@ class WxApplication(object):
             'SCAN': self.on_scan,
             'LOCATION': self.on_location_update,
             'CLICK': self.on_click,
-            'VIEW': self.on_view
+            'VIEW': self.on_view,
+            'scancode_push': self.on_scancode_push,
+            'scancode_waitmsg': self.on_scancode_waitmsg,
+            'pic_sysphoto': self.on_pic_sysphoto,
+            'pic_photo_or_album': self.on_pic_photo_or_album,
+            'pic_weixin': self.on_pic_weixin,
+            'location_select': self.on_location_select,
         }
 
     def on_event(self, event):
@@ -236,19 +132,37 @@ class WxApplication(object):
         return WxTextResponse(self.WELCOME_TXT, sub)
 
     def on_unsubscribe(self, unsub):
-        return WxTextResponse(self.UNSUPPORT_TXT, unsub)
+        return WxEmptyResponse()
 
     def on_click(self, click):
-        return WxTextResponse(self.UNSUPPORT_TXT, click)
+        return WxEmptyResponse()
 
     def on_scan(self, scan):
-        return WxTextResponse(self.UNSUPPORT_TXT, scan)
+        return WxEmptyResponse()
 
     def on_location_update(self, location):
-        return WxTextResponse(self.UNSUPPORT_TXT, location)
+        return WxEmptyResponse()
 
     def on_view(self, view):
-        return WxTextResponse(self.UNSUPPORT_TXT, view)
+        return WxEmptyResponse()
+
+    def on_scancode_push(self, event):
+        return WxEmptyResponse()
+
+    def on_scancode_waitmsg(self, event):
+        return WxEmptyResponse()
+
+    def on_pic_sysphoto(self, event):
+        return WxEmptyResponse()
+
+    def on_pic_photo_or_album(self, event):
+        return WxEmptyResponse()
+
+    def on_pic_weixin(self, event):
+        return WxEmptyResponse()
+
+    def on_location_select(self, event):
+        return WxEmptyResponse()
 
     def handler_map(self):
         if getattr(self, 'handlers', None):
@@ -268,13 +182,6 @@ class WxApplication(object):
 
     def post_process(self, rsp):
         pass
-
-
-class APIError(object):
-
-    def __init__(self, code, msg):
-        self.code = code
-        self.message = msg
 
 
 class WxApi(object):
@@ -487,3 +394,12 @@ class WxApi(object):
 
     def delete_menu(self):
         return self._get('menu/delete')
+
+    def customservice_records(self, starttime, endtime, openid=None,
+                              pagesize=100, pageindex=1):
+        return self._get('customservice/getrecord',
+                         {'starttime': starttime,
+                          'endtime': endtime,
+                          'openid': openid,
+                          'pagesize': pagesize,
+                          'pageindex': pageindex})
